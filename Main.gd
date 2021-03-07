d5b925f9a454b5e6e1950ff29be97e565a4604cb
Paul Sheridan <paul.sheridan@soartech.com>
2021-03-07 12:38:28 -0500

    mostly fixed text staying

--- Main.gd
+++ Main.gd
@@ -21,7 +21,7 @@ func new_game():
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	#$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
+	$HUD.show_message("")
 	$Music.play()
 
 
